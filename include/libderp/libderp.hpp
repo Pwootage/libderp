6b6a0f153bb35ba88412c7e2dc5510fb7d8a117c
Pwootage <pwootage@gmail.com>
2017-02-19 12:51:23 -0700

    Initial commit

---
+++ include/libderp/libderp.hpp
@@ -0,0 +1,6 @@
+#ifndef LIBDERP_LIBRARY_H
+#define LIBDERP_LIBRARY_H
+
+void hello();
+
+#endif
---
+++ src/libderp.cpp
@@ -0,0 +1,7 @@
+#include "libderp/libderp.hpp"
+
+#include <iostream>
+
+void hello() {
+  std::cout << "Hello, World!" << std::endl;
+}
